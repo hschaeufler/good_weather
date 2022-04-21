5c84610d1c2eff2cb7195bdf82e76171890892b8
Holger Schäufler <holger.schaeufler@hallesche.de>
2022-04-21 17:19:31 +0200

    Update generated City-Model

--- lib/models/city.g.dart
+++ lib/models/city.g.dart
@@ -6,15 +6,16 @@ part of 'city.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-City _$CityFromJson(Map<String, dynamic> json) => City()
-  ..name = json['name'] as String
-  ..local_names = (json['local_names'] as Map<String, dynamic>?)?.map(
-    (k, e) => MapEntry(k, e as String),
-  )
-  ..lat = (json['lat'] as num).toDouble()
-  ..lon = (json['lon'] as num).toDouble()
-  ..country = json['country'] as String
-  ..state = json['state'] as String;
+City _$CityFromJson(Map<String, dynamic> json) => City(
+      name: json['name'] as String,
+      local_names: (json['local_names'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, e as String),
+      ),
+      lat: (json['lat'] as num).toDouble(),
+      lon: (json['lon'] as num).toDouble(),
+      country: json['country'] as String,
+      state: json['state'] as String,
+    );
 
 Map<String, dynamic> _$CityToJson(City instance) => <String, dynamic>{
       'name': instance.name,
