06e59fbe1f6affe0d7058858920e00cdbeff4a49
Holger Schäufler <holger.schaeufler@hallesche.de>
2022-04-22 16:05:16 +0200

    Add Environment-Constants

---
+++ lib/utils/environment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static String get envFileName => kReleaseMode ? "prod.env" : "dev.env";
+  static String get apiToken => dotenv.env['API_TOKEN'] ?? '';
+  static String get apiEndpoint => dotenv.env['API_ENDPOINT'] ?? '';
+  static String get geoCodeAPIPath => dotenv.env['GEOCODE_API_PATH'] ?? '';
+  static String get geoCodeBaseURL => apiEndpoint + geoCodeAPIPath;
+}
