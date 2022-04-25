c814670629d5e589217e5173dfdae84d701bf7b7
Holger Schäufler <holger.schaeufler@hallesche.de>
2022-04-25 09:17:03 +0200

    Update Path od .env-File

--- lib/utils/environment.dart
+++ lib/utils/environment.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
-  static String get envFileName => kReleaseMode ? "prod.env" : "dev.env";
+  static String get envFileName => "assets/env/" + (kReleaseMode ? "prod.env" : "dev.env");
   static String get apiToken => dotenv.env['API_TOKEN'] ?? '';
   static String get apiEndpoint => dotenv.env['API_ENDPOINT'] ?? '';
   static String get geoCodeAPIPath => dotenv.env['GEOCODE_API_PATH'] ?? '';
