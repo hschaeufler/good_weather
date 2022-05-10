4a0e3f969bc543a94216a6509a3e1ac15b209f0c
Holger Schäufler <holger.schaeufler@hallesche.de>
2022-05-10 18:06:46 +0200

    Add Weather-Service as abstraction Layer

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,38 @@
+import '../models/city.dart';
+import '../models/weather_data.dart';
+import '../repositories/city_repository.dart';
+import '../repositories/weather_repository.dart';
+
+class WeatherService {
+
+  WeatherService._();
+
+  static final WeatherService _instance = WeatherService._();
+
+  factory WeatherService() {
+    return _instance;
+  }
+
+  final CityRepository _cityRepository = CityRepository();
+  final WeatherRepository _weatherRepository = WeatherRepository();
+
+  Future<WeatherData> getWeatherByCityId(int cityId) {
+    return _cityRepository
+        .getById(cityId)
+        .then((city) => _weatherRepository.getCurrentWeather(city));
+  }
+
+  Future<List<City>> getAllCities() {
+    return _cityRepository.getAllCities();
+  }
+
+  Future<List<City>> deleteAllCities() async {
+    await _cityRepository.deleteAllCities();
+    return _cityRepository.getAllCities();
+  }
+
+  Future<List<City>> deleteCity(City city) async {
+    await _cityRepository.deleteCity(city);
+    return _cityRepository.getAllCities();
+  }
+}
