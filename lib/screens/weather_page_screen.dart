5a125e51d5bbfacc0185e4fbe838db5462fe494a
Holger Schäufler <holger.schaeufler@hallesche.de>
2022-06-23 14:35:38 +0200

    Implement Parallax-Effect

--- lib/screens/weather_page_screen.dart
+++ lib/screens/weather_page_screen.dart
@@ -23,6 +23,8 @@ class _WeatherPageScreenState extends State<WeatherPageScreen> {
 
   Future<List<Object>>? weatherData;
 
+  double scrollFraction = 0;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -39,36 +41,63 @@ class _WeatherPageScreenState extends State<WeatherPageScreen> {
             onRefresh: () async {
               _fetchWeatherData();
             },
-            child: Container(
-              child: DraggableScrollableSheet(
-                  minChildSize: 0.5,
-                  builder: (BuildContext context,
-                      ScrollController scrollController) {
-                    return ListView(
-                      controller: scrollController,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Weather(weatherData: weather),
+            child: NotificationListener<ScrollNotification>(
+              onNotification: (scrollNotification) {
+                print("Pixels: ${scrollNotification.metrics.pixels}");
+                print("Max: ${scrollNotification.metrics.maxScrollExtent}");
+                print(
+                    "Viewport: ${scrollNotification.metrics.viewportDimension}");
+                setState(() {
+                  scrollFraction = ((scrollNotification.metrics.maxScrollExtent -
+                          scrollNotification.metrics.pixels) /
+                      scrollNotification.metrics.viewportDimension).clamp(0.0, 1.0);
+                });
+                print("diff: ${scrollFraction}");
+                return false;
+              },
+              child: Stack(
+                children: [
+                  Transform.scale(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(image.mobile),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              top: 25, bottom: 25, left: 10, right: 10),
-                          child: HourlyForecastListView(
-                              hourlyForecastList: fullWeather.hourlyForecast),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              top: 25, bottom: 25, left: 10, right: 10),
-                          child: DailyForecastListView(
-                              dailyForecastList: fullWeather.dailyForecast),
-                        ),
-                      ],
-                    );
-                  }),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(image.mobile), fit: BoxFit.cover),
+                      ),
+                    ),
+                    scale: 1.2,
+                    alignment: Alignment(0, scrollFraction * 2 - 1),
+                    //alignment: Alignment.topCenter,
+                  ),
+                  DraggableScrollableSheet(
+                      minChildSize: 0.5,
+                      builder: (BuildContext context,
+                          ScrollController scrollController) {
+                        return ListView(
+                          controller: scrollController,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Weather(weatherData: weather),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 25, bottom: 25, left: 10, right: 10),
+                              child: HourlyForecastListView(
+                                  hourlyForecastList:
+                                      fullWeather.hourlyForecast),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 25, bottom: 25, left: 10, right: 10),
+                              child: DailyForecastListView(
+                                  dailyForecastList: fullWeather.dailyForecast),
+                            ),
+                          ],
+                        );
+                      }),
+                ],
               ),
             ),
           );
